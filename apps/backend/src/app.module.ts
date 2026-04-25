120a3dd8a47ed1de21c721ca94b76854b303d2a1
Francisco Gustavo <gustavo@frcgustavo.com>
2026-04-25 15:23:59 -0600

    feat(backend): configure typeorm backend infrastructure

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,25 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { ClientsModule } from './clients/clients.module';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
-import { PriceListsModule } from './price-lists/price-lists.module';
-import { ProductPricesModule } from './product-prices/product-prices.module';
 import { OrdersModule } from './orders/orders.module';
 import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
   imports: [
-    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number.parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? 'postgres',
+      database: process.env.DB_DATABASE ?? 'vegyfresh',
+      autoLoadEntities: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
     ClientsModule,
     UsersModule,
     RolesModule,
     SuppliersModule,
-    PriceListsModule,
-    ProductPricesModule,
     OrdersModule,
     CatalogModule,
   ],
