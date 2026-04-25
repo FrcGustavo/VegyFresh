ba52ee24ca99e5a5967a4a592bcb86b21708f6a0
Francisco Gustavo <gustavo@frcgustavo.com>
2026-04-25 13:35:47 -0600

    feat(backend): add all modules
    Signed-off-by: Francisco Gustavo <gustavo@frcgustavo.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { PriceListsModule } from './price-lists/price-lists.module';
+import { ProductPricesModule } from './product-prices/product-prices.module';
+import { OrdersModule } from './orders/orders.module';
+import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule,
+    ClientsModule,
+    UsersModule,
+    RolesModule,
+    SuppliersModule,
+    PriceListsModule,
+    ProductPricesModule,
+    OrdersModule,
+    CatalogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
